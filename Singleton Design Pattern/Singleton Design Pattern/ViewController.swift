231f206c56f105f2daadfb5cb2623ec562e3de66
Bathi Babu Doddi <bathibabu.doddi67@gmail.com>
2020-07-08 11:48:15 +0530

    Initial commit

---
+++ Singleton Design Pattern/Singleton Design Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Singleton Design Pattern
+//
+//  Created by Bathi Babu on 08/07/20.
+//  Copyright © 2020 Bathi Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
