6bc63539447784addf180ed7c8546c1e641c0594
Bathi Babu Doddi <bathibabu.doddi67@gmail.com>
2020-07-08 22:56:16 +0530

    Builder initial commit

---
+++ Builder Design Pattern/Builder Design Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Builder Design Pattern
+//
+//  Created by Bathi Babu on 08/07/20.
+//  Copyright © 2020 Bathi Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Builder Design Pattern/Builder Design PatternTests/Builder_Design_PatternTests.swift
@@ -0,0 +1,34 @@
+//
+//  Builder_Design_PatternTests.swift
+//  Builder Design PatternTests
+//
+//  Created by Bathi Babu on 08/07/20.
+//  Copyright © 2020 Bathi Babu. All rights reserved.
+//
+
+import XCTest
+@testable import Builder_Design_Pattern
+
+class Builder_Design_PatternTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
